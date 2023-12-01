1adbd759790417ef5397d0db660aac188dadc792
Ricardo Esteves <ricardoglsesteves@gmail.com>
2023-12-01 07:18:44 +0000

    chore: :heavy_plus_sign: Lodash
    add loadsh

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -3,7 +3,7 @@ import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 
 import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
-// import { FormPopover } from "@/components/form/form-popover";
+import { FormPopover } from "@/components/form/form-popover";
 
 import { MobileSidebar } from "./mobile-sidebar";
 
@@ -15,24 +15,24 @@ export const Navbar = () => {
         <div className="hidden md:flex">
           <Logo />
         </div>
-        {/* <FormPopover align="start" side="bottom" sideOffset={18}> */}
-        <Button
-          variant="primary"
-          size="sm"
-          className="rounded-sm hidden md:block h-auto  py-1.5 px-2"
-        >
-          Create
-        </Button>
-        {/* </FormPopover> */}
-        {/* <FormPopover> */}
-        <Button
-          variant="primary"
-          size="sm"
-          className="rounded-sm block md:hidden"
-        >
-          <Plus className="h-4 w-4" />
-        </Button>
-        {/* </FormPopover> */}
+        <FormPopover align="start" side="bottom" sideOffset={18}>
+          <Button
+            variant="primary"
+            size="sm"
+            className="rounded-sm hidden md:block h-auto  py-1.5 px-2"
+          >
+            Create
+          </Button>
+        </FormPopover>
+        <FormPopover>
+          <Button
+            variant="primary"
+            size="sm"
+            className="rounded-sm block md:hidden"
+          >
+            <Plus className="h-4 w-4" />
+          </Button>
+        </FormPopover>
       </div>
       <div className="ml-auto flex items-center gap-x-2">
         <OrganizationSwitcher
--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,3 +1,4 @@
+import { startCase } from "lodash";
 import { auth } from "@clerk/nextjs";
 
 import { OrgControl } from "./_components/org-control";
@@ -5,7 +6,9 @@ import { OrgControl } from "./_components/org-control";
 export async function generateMetadata() {
   const { orgSlug } = auth();
 
-  return {};
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
 }
 
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
