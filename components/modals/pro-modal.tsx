7c9af8a8b3e9b4827cb26f1bb1e6f981d6349447
Ricardo Esteves <ricardoglsesteves@gmail.com>
2023-12-04 01:30:49 +0000

    chore: :adhesive_bandage: Setup the stripe portal
    - Finish and test stripe integration. - Add codeql config .
    - Change dependabot config .
    - Add image for the pro modal .

--- components/modals/pro-modal.tsx
+++ components/modals/pro-modal.tsx
@@ -29,14 +29,12 @@ export const ProModal = () => {
     <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
       <DialogContent className="max-w-md p-0 overflow-hidden">
         <div className="aspect-video relative flex items-center justify-center">
-          <Image src="/hero.svg" alt="Hero" className="object-cover" fill />
+          <Image src="/hero.png" alt="Hero" fill />
         </div>
         <div className="text-neutral-700 mx-auto space-y-6 p-6">
-          <h2 className="font-semibold text-xl">
-            Upgrade to SprintEase Pro Today!
-          </h2>
+          <h2 className="font-semibold text-xl">Upgrade to Pro Today!</h2>
           <p className="text-xs font-semibold text-neutral-600">
-            Explore the best of SprintEase
+            More work, less time.
           </p>
           <div className="pl-3">
             <ul className="text-sm list-disc">
